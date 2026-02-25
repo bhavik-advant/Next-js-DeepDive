49395d59429e9df9977822ac35dbd9a134d07e39
bhavik-advant <bhavik@advant.xyz>
2026-02-25 19:58:16 +0530

    Initialize Next.js application ,basic structure, and global styles. Added .gitignore, package.json, and essential pages including Home, About, and Blog. Included header component and global CSS for styling.

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import Header from './components/header';
+
+export default function Home() {
+  return (
+    <main>
+      <Header />
+      <p>🔥 Let&apos;s get started! 🔥</p>
+      <p><Link href="/about">About US</Link></p>
+    </main>
+  );
+}
